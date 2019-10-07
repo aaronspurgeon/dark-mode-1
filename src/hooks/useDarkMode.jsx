6928aeebe7a2fa900287158c3b2a84f912b524a0
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-10-07 19:36:39 -0400

    Added in the darkmode hook, havent finished it yet, i started this project on the fork of lambdas then converted it to my own repo so i get credit for the contributions

---
+++ src/hooks/useDarkMode.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react';
+import useLocalStorage from './useLocalStorage';
+
+export const useDarkMode = () => {
+    const [someValue, setSomeValue] = useLocalStorage('toggle');
+    useEffect()
+}
